f6a69c308cc728507e1c37fe53e1c80d0b1c85c9
Felipe Segovia <segoviamg@gmail.com>
2023-09-02 12:41:48 -0300

    criação do handler

---
+++ api/handler.py
@@ -0,0 +1,57 @@
+import pickle
+import pandas as pd
+from flask import Flask, request, Response
+import requests
+from rossmann.Rossmann import Rossmann
+import json
+
+
+
+#loading model
+model = pickle.load(open ('/home/segovia/repos/rosmann_project/models/model_rossmann.pkl', 'rb'))
+
+#initialize API
+app = Flask(__name__)
+
+@app.route(  '/rossmann/predict', methods = ['POST'])
+def rossmann_predict():
+    test_json = request.get_json()
+  
+    if test_json:
+
+        if isinstance(test_json, dict):
+            test_raw = pd.DataFrame(test_json, index=[0])
+
+        else:
+            test_raw = pd.DataFrame(test_json, columns=test_json[0].keys())
+
+        #instantiate rossmann class
+        pipeline = Rossmann()
+
+        #data cleaning
+        df1 = pipeline.data_cleaning(test_raw)
+
+        #feature engineering
+
+        df2 = pipeline.feature_engineering(df1)
+
+        #data preparation
+
+        df3 = pipeline.data_preparation(df2)
+
+        #prediction
+
+        df_response = pipeline.get_prediction(model, test_raw, df3)
+
+        return df_response
+
+
+
+
+    else:
+        return Response('{}', status=200, mimetype='application/json')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', debug=True)
+
